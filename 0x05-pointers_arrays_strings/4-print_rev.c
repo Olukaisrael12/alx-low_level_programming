cccc058fe0ae189368164e96f75c7e2e24b08a23
olukaisrael12 <olukaisrael7@gmail.com>
2023-09-19 04:16:53 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - function prints string in reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int longl = 0;
+	int o;
+
+	while
+		(*s != '\0')
+		{
+			longl++;
+			s++;
+		}
+	s--;
+	for
+		(o = longl;
+	o > 0;)
+	o++;
+	{
+		_putchar('\n');
+	}
+}
